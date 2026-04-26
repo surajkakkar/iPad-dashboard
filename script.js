a501eb8eaf1fd787610b58cf5f7fa0a2d8d289d2
surajkakkar <suraj.kakkar12@gmail.com>
2026-04-25 20:06:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function updateClock() {
+  const now = new Date();
+  document.getElementById("clock").innerText =
+    now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
